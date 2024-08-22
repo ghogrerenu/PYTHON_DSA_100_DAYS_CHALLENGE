4ccdf8aaba0348874d47f66f939e6280393facd4
ghogrerenu <165774924+ghogrerenu@users.noreply.github.com>
2024-08-22 21:39:11 +0530

    Add files via upload

---
+++ w5-a1-q6.py
@@ -0,0 +1,17 @@
+'''Q7. Python program to find the size of a Dictionary. Basically print how
+many total key-value pair are there.'''
+
+# Define the dictionary
+my_dict = {
+    'Physics': 85,
+    'Chemistry': 90,
+    'Mathematics': 95,
+    'Biology': 88,
+    'English': 78
+}
+
+# Find the size of the dictionary
+size = len(my_dict)
+
+# Print the size of the dictionary
+print(f"The total number of key-value pairs in the dictionary is {size}.")
